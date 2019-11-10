272df5886286e85f155dd4546713bea03ea7a290
SimonAble <simonbull1@gmail.com>
2019-11-10 12:07:47 -0500

    Added Concept Model for Dynamic Concept View

--- src/app/subjectFocusViews/concept-view/concept-view-model.component.ts
+++ src/app/subjectFocusViews/concept-view/concept-view-model.component.ts
@@ -1,12 +1,29 @@
 export class ConceptModel {
   conceptHeading: string;
-  conceptArray: Concept[];
+  conceptFormula: string;
+  conceptParagraphs: ConceptParagraph[];
+  conceptInteraction: string;
+  conceptImg: any;
+
+  constructor(conceptHeading: string, conceptFormula: string, conceptParagraphs: ConceptParagraph[], conceptInteraction?: string, conceptImg?: any) {
+    this.conceptHeading = conceptHeading;
+    this.conceptFormula = conceptFormula;
+    this.conceptParagraphs = conceptParagraphs;
+    this.conceptInteraction = conceptInteraction;
+    this.conceptImg = conceptImg;
+  }
 }
 
-export class Concept {
-  conceptSubheading: string;
-  conceptText: string;
-  conceptImg: any;
+export class ConceptParagraph {
+  paragraphText: string;
+  paragraphImg: string;
+  paragraphImgAlt: string;
+
+  constructor(paragraphText: string, paragraphImg?: string, paragraphImgAlt?: string) {
+    this.paragraphText = paragraphText;
+    this.paragraphImg = paragraphImg;
+    this.paragraphImgAlt = paragraphImgAlt;
+  }
 }
 
 
--- src/app/subjectFocusViews/concept-view/concept-view.component.ts
+++ src/app/subjectFocusViews/concept-view/concept-view.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ConceptModel, Concept } from './concept-view-model.component';
+import { ConceptModel, ConceptParagraph } from './concept-view-model.component';
 
 @Component({
   selector: 'app-concept-view',
@@ -7,7 +7,14 @@ import { ConceptModel, Concept } from './concept-view-model.component';
   styleUrls: ['./concept-view.component.css']
 })
 export class ConceptViewComponent implements OnInit {
-  public conceptModel: ConceptModel[] = [];
+  public conceptModel: ConceptModel = new ConceptModel(
+    'Whole Numbers',
+    '1, 2, 3, 4, 5, ...',
+    [
+      new ConceptParagraph('Even though counting is first taught at a young age, mastering mathematics, which is the study of numbers, requires  constant attention. If it has been a while since you have studied math, it can be helpful to review basic topics. In  this chapter, we will focus on numbers used for counting as well as four arithmetic operations—addition, subtraction,  multiplication, and division. We will also discuss some vocabulary that we will use throughout this book.')
+    ]
+
+  )
 
   constructor() { }
 
